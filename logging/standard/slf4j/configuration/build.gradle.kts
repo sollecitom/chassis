9332365494b1c3c8e0bf041f77ea502a70678afe
Michele Sollecito <michele.sollecito@gmail.com>
2023-07-29 08:39:55 +0100

    Standard SLF4J logging configuration

---
+++ logging/standard/slf4j/configuration/build.gradle.kts
@@ -0,0 +1,7 @@
+dependencies {
+    api(projects.chassisLoggingStandardConfiguration)
+    runtimeOnly(projects.chassisLoggerSlf4jAdapter)
+
+    testImplementation(projects.chassisTestUtils)
+    testImplementation(projects.chassis.chassisJsonTestUtils)
+}
