b5f78058cdc8e936a557eea73eb766ea99bf5abb
Michele Sollecito <michele.sollecito@gmail.com>
2023-08-01 16:55:51 +0100

    WIP

--- cryptography/domain/src/main/kotlin/org/sollecitom/chassis/cryptography/domain/symmetric/algorithms/aes/AES.kt
+++ cryptography/domain/src/main/kotlin/org/sollecitom/chassis/cryptography/domain/symmetric/algorithms/aes/AES.kt
@@ -2,7 +2,7 @@ package org.sollecitom.chassis.cryptography.domain.symmetric.algorithms.aes
 
 import org.sollecitom.chassis.cryptography.domain.algorithm.Algorithm
 
-// TODO create an EncryptionAlgorithm if needed
+// TODO create an EncryptionAlgorithm and a SymmetricAlgorithm if needed
 object AES : Algorithm {
 
     override val name = "AES"
