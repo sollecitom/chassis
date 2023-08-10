af9054859768265759b28003939f5908e864e47a
Michele Sollecito <michele.sollecito@gmail.com>
2023-08-10 23:08:25 +0100

    WIP

--- example/webapp/kweb/starter/src/main/kotlin/org/sollecitom/chassis/example/webapp/kweb/starter/Starter.kt
+++ example/webapp/kweb/starter/src/main/kotlin/org/sollecitom/chassis/example/webapp/kweb/starter/Starter.kt
@@ -2,7 +2,6 @@ package org.sollecitom.chassis.example.webapp.kweb.starter
 
 import kotlinx.coroutines.coroutineScope
 import kweb.*
-import kweb.routing.PathReceiver
 import kweb.state.KVar
 import org.http4k.cloudnative.env.Environment
 import org.sollecitom.chassis.configuration.utils.formatted
@@ -25,39 +24,35 @@ suspend fun main() = coroutineScope<Unit> {
     }
 }
 
-class PathIdVisualizer(private val idType: String, private val listIdParamName: String) : RoutedComponent {
+interface RoutedComponentTemplate : RoutedComponent {
 
-    override fun invoke(html: ElementCreator<*>, params: Map<String, KVar<String>>) {
-        with(html) {
-            val listId = params.getValue(listIdParamName)
-            h1().text(listId.map { "$idType id: $it" })
-        }
+    fun ElementCreator<*>.render(params: Map<String, KVar<String>>)
+
+    override fun invoke(html: ElementCreator<*>, params: Map<String, KVar<String>>) = with(html) { render(params) }
+}
+
+class PathIdVisualizer(private val idType: String, private val listIdParamName: String) : RoutedComponentTemplate {
+
+    override fun ElementCreator<*>.render(params: Map<String, KVar<String>>) {
+
+        val listId = params.getValue(listIdParamName)
+        h1().text(listId.map { "$idType id: $it" })
     }
 }
 
 typealias RoutedComponent = (ElementCreator<*>, Map<String, KVar<String>>) -> Unit
 
-class MainPage : RoutedComponent {
+class MainPage : RoutedComponentTemplate {
 
-    override fun invoke(html: ElementCreator<*>, params: Map<String, KVar<String>>) {
-        with(html) {
-            p().text("What is your name?")
-            val input = input(type = InputType.text)
-            input.value = KVar("Peter Pan")
-            val greeting = input.value.map { name -> "Hi $name!" }
-            p().text(greeting)
-        }
+    override fun ElementCreator<*>.render(params: Map<String, KVar<String>>) {
+
+        p().text("What is your name?")
+        val input = input(type = InputType.text)
+        input.value = KVar("Peter Pan")
+        val greeting = input.value.map { name -> "Hi $name!" }
+        p().text(greeting)
     }
 }
-
-fun mainPage(): PathReceiver = {
-    p().text("What is your name?")
-    val input = input(type = InputType.text)
-    input.value = KVar("Peter Pan")
-    val greeting = input.value.map { name -> "Hi $name!" }
-    p().text(greeting)
-}
-
 
 private object Starter : Loggable()
 
