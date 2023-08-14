48c5a4f239aa348cad7d3ed6364d06366a67bb6b
Michele Sollecito <michele.sollecito@gmail.com>
2023-08-14 15:25:29 +0100

    WIP

--- example/webapp/vaadin/adapters/driving/web/src/main/kotlin/org/sollecitom/chassis/example/webapp/vaadin/adapters/driving/web/VaadinAppConfiguration.kt
+++ example/webapp/vaadin/adapters/driving/web/src/main/kotlin/org/sollecitom/chassis/example/webapp/vaadin/adapters/driving/web/VaadinAppConfiguration.kt
@@ -3,12 +3,13 @@ package org.sollecitom.chassis.example.webapp.vaadin.adapters.driving.web
 import com.vaadin.flow.component.dependency.NpmPackage
 import com.vaadin.flow.component.page.AppShellConfigurator
 import com.vaadin.flow.server.PWA
+import com.vaadin.flow.spring.annotation.EnableVaadin
 import com.vaadin.flow.theme.Theme
-import org.springframework.web.context.annotation.ApplicationScope
+import org.springframework.context.annotation.Configuration
 
 @NpmPackage(value = "lumo-css-framework", version = "^4.0.10")
 @Theme("flowcrmtutorial")
 @PWA(name = "VaadinCRM", shortName = "CRM", offlinePath = "offline.html", offlineResources = ["./images/offline.png"])
-@ApplicationScope // TODO try to remove this
-class VaadinAppConfigurator : AppShellConfigurator
-
+@Configuration
+@EnableVaadin("org.sollecitom.chassis.example.webapp.vaadin.adapters.driving.web.*")
+open class VaadinAppConfiguration : AppShellConfigurator
--- example/webapp/vaadin/adapters/driving/web/src/main/kotlin/org/sollecitom/chassis/example/webapp/vaadin/adapters/driving/web/VaadinConfiguration.kt
+++
@@ -1,8 +0,0 @@
-package org.sollecitom.chassis.example.webapp.vaadin.adapters.driving.web
-
-import com.vaadin.flow.spring.annotation.EnableVaadin
-import org.springframework.context.annotation.Configuration
-
-@Configuration
-@EnableVaadin("org.sollecitom.chassis.example.webapp.vaadin.adapters.driving.web.views")
-open class VaadinConfiguration
