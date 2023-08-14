8619de352e8537fddadf1bc52f27187f7a39467d
Michele Sollecito <michele.sollecito@gmail.com>
2023-08-14 15:28:57 +0100

    WIP

--- example/webapp/vaadin/adapters/driving/web/src/main/kotlin/org/sollecitom/chassis/example/webapp/vaadin/adapters/driving/web/VaadinAppConfiguration.kt
+++ example/webapp/vaadin/adapters/driving/web/src/main/kotlin/org/sollecitom/chassis/example/webapp/vaadin/adapters/driving/web/VaadinAppConfiguration.kt
@@ -7,9 +7,9 @@ import com.vaadin.flow.spring.annotation.EnableVaadin
 import com.vaadin.flow.theme.Theme
 import org.springframework.context.annotation.Configuration
 
-@NpmPackage(value = "lumo-css-framework", version = "^4.0.10")
+@NpmPackage(value = "lumo-css-framework", version = "^4.0.10") // TODO fix this
 @Theme("flowcrmtutorial")
 @PWA(name = "VaadinCRM", shortName = "CRM", offlinePath = "offline.html", offlineResources = ["./images/offline.png"])
 @Configuration
-@EnableVaadin("org.sollecitom.chassis.example.webapp.vaadin.adapters.driving.web.*")
+@EnableVaadin("org.sollecitom.chassis.example.webapp.vaadin.adapters.driving.web")
 open class VaadinAppConfiguration : AppShellConfigurator
---
+++ example/webapp/vaadin/configuration/src/main/kotlin/org/sollecitom/chassis/example/webapp/vaadin/configuration/Packages.kt
@@ -0,0 +1,7 @@
+package org.sollecitom.chassis.example.webapp.vaadin.configuration
+
+object Packages {
+
+    const val BASE = "org.sollecitom.chassis.example.webapp.vaadin"
+    const val WEB_DRIVING_ADAPTER = "org.sollecitom.chassis.example.webapp.vaadin.adapters.driving.web"
+}
