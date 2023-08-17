ea44936b4b5c46299920f7acabe17b57b88761f6
Michele Sollecito <michele.sollecito@gmail.com>
2023-08-17 13:59:48 +0100

    WIP

--- example/service/write-endpoint/starter/build.gradle.kts
+++ example/service/write-endpoint/starter/build.gradle.kts
@@ -1,7 +1,11 @@
 import com.google.cloud.tools.jib.api.buildplan.ImageFormat
 import com.google.cloud.tools.jib.gradle.JibExtension
+import com.google.cloud.tools.jib.gradle.PlatformParameters
+import com.google.cloud.tools.jib.gradle.PlatformParametersSpec
 import com.palantir.gradle.gitversion.VersionDetails
 import groovy.lang.Closure
+import org.gradle.nativeplatform.platform.internal.ArchitectureInternal
+import org.gradle.nativeplatform.platform.internal.DefaultNativePlatform
 import java.time.ZoneOffset
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
@@ -44,6 +48,8 @@ val mainAppPort = "8081"
 val healthAppPort = "8082"
 val containerEnvironment = mutableMapOf("SERVICE_PORT" to mainAppPort, "HEALTH_PORT" to healthAppPort, "BUILD_REVISION" to gitVersionDetails.gitHashFull, "BUILD_TIMESTAMP" to buildTimestamp)
 val imageTags = setOf(gitVersionDetails.gitHashFull, "snapshot")
+val currentOperatingSystem: OperatingSystem = DefaultNativePlatform.getCurrentOperatingSystem()
+val currentArchitecture: ArchitectureInternal = DefaultNativePlatform.getCurrentArchitecture()
 
 configure<JibExtension> {
     container {
@@ -55,8 +61,7 @@ configure<JibExtension> {
         format = ImageFormat.OCI
         labels.set(
             mapOf(
-                "build.git.hash.full" to gitVersionDetails.gitHashFull,
-                "build.timestamp" to buildTimestamp
+                "build.git.hash.full" to gitVersionDetails.gitHashFull, "build.timestamp" to buildTimestamp
             )
         )
         filesModificationTime.set(buildTimestamp)
@@ -65,9 +70,32 @@ configure<JibExtension> {
     }
     from {
         image = dockerBaseImage
+        platforms {
+            configureForOperatingSystem(currentOperatingSystem, currentArchitecture)
+        }
     }
     to {
         image = serviceImage
         tags = imageTags
     }
+}
+
+// TODO move to a library and import it
+fun PlatformParametersSpec.configureForOperatingSystem(currentOS: OperatingSystem, currentArchitecture: ArchitectureInternal) {
+
+    // TODO improve this "sophisticated" approach :-)
+    when {
+        currentOS.isMacOsX && currentArchitecture.isArm64 -> platform { appleSilicon() }
+        else -> platform { linux() }
+    }
+}
+
+fun PlatformParameters.appleSilicon() {
+    architecture = "arm64"
+    os = "linux"
+}
+
+fun PlatformParameters.linux() {
+    architecture = "amd64"
+    os = "linux"
 }
