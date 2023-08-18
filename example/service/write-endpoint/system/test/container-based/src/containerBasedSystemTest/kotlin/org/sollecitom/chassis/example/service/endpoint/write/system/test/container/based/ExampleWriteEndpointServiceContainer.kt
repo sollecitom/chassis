f374c67c7edb886fbe55d529ebbfef362c0d4afc
Michele Sollecito <michele.sollecito@gmail.com>
2023-08-18 15:17:56 +0100

    WIP

--- example/service/write-endpoint/system/test/container-based/src/containerBasedSystemTest/kotlin/org/sollecitom/chassis/example/service/endpoint/write/system/test/container/based/ExampleWriteEndpointServiceContainer.kt
+++ example/service/write-endpoint/system/test/container-based/src/containerBasedSystemTest/kotlin/org/sollecitom/chassis/example/service/endpoint/write/system/test/container/based/ExampleWriteEndpointServiceContainer.kt
@@ -1,64 +1,45 @@
 package org.sollecitom.chassis.example.service.endpoint.write.system.test.container.based
 
-import org.sollecitom.chassis.example.service.endpoint.write.configuration.ApplicationProperties
+import org.sollecitom.chassis.example.service.endpoint.write.configuration.ApplicationProperties.HEALTH_PORT
+import org.sollecitom.chassis.example.service.endpoint.write.configuration.ApplicationProperties.SERVICE_OCI_IMAGE_NAME
+import org.sollecitom.chassis.example.service.endpoint.write.configuration.ApplicationProperties.SERVICE_OCI_IMAGE_REPOSITORY
+import org.sollecitom.chassis.example.service.endpoint.write.configuration.ApplicationProperties.SERVICE_PORT
+import org.sollecitom.chassis.example.service.endpoint.write.configuration.ApplicationProperties.SERVICE_STARTED_LOG_MESSAGE
+import org.sollecitom.chassis.logging.standard.configuration.StandardLoggingConfiguration.Properties.defaultMinimumLoggingLevelEnvironmentVariableName
+import org.sollecitom.chassis.logging.standard.configuration.StandardLoggingConfiguration.Properties.defaultMinimumLoggingLevelOverridesEnvironmentVariableName
 import org.sollecitom.chassis.web.service.domain.WebServiceInfo
 import org.testcontainers.containers.GenericContainer
-import org.testcontainers.containers.wait.strategy.HttpWaitStrategy
+import org.testcontainers.containers.wait.strategy.LogMessageWaitStrategy
 import org.testcontainers.containers.wait.strategy.Wait
 import org.testcontainers.utility.DockerImageName
 
-// TODO refactor this whole mess
-private const val IMAGE = "example-write-endpoint:snapshot"
-private const val IMAGE_REPOSITORY = "ghcr.io/sollecitom-chassis/"
-private val imageName = DockerImageName.parse("$IMAGE_REPOSITORY$IMAGE")
-
 fun newExampleWriteEndpointServiceContainer(servicePort: Int = 8090, healthPort: Int = 8091): ExampleWriteEndpointServiceContainer {
 
-    val loggingArguments = mapOf("LOGGING_LEVEL_DEFAULT" to "INFO")
-    val webArguments = mapOf("SERVICE_PORT" to "$servicePort", "HEALTH_PORT" to "$healthPort", "LOGGING_LEVELS" to "io.micronaut.web.router=INFO")
+    val loggingArguments = mapOf(defaultMinimumLoggingLevelEnvironmentVariableName to "INFO", defaultMinimumLoggingLevelOverridesEnvironmentVariableName to "org.eclipse.jetty=WARN,org.apache.hc=WARN")
+    val webArguments = mapOf(SERVICE_PORT to "$servicePort", HEALTH_PORT to "$healthPort")
     val arguments = (webArguments + loggingArguments)
-    // TODO refactor this
-    val waitStrategy = Wait.forLogMessage(".*${ApplicationProperties.SERVICE_STARTED_LOG_MESSAGE}.*", 1)
-    return ExampleWriteEndpointServiceContainer(servicePort, healthPort).withExposedPorts(servicePort, healthPort).waitingFor(waitStrategy).withJavaArgs(arguments)
+    return ExampleWriteEndpointServiceContainer(servicePort, healthPort).withExposedPorts(servicePort, healthPort).waitingFor(waitForServiceStartedLogMessage()).withJavaArgs(arguments)
 }
 
-// TODO fix the wait strategy so that it uses the readiness check or a log statement
-class ExampleWriteEndpointServiceContainer(val servicePort: Int, val healthPort: Int) : GenericContainer<ExampleWriteEndpointServiceContainer>(imageName) {
+private fun waitForServiceStartedLogMessage(): LogMessageWaitStrategy = Wait.forLogMessage(".*$SERVICE_STARTED_LOG_MESSAGE.*", 1)
 
-    val webServiceInfo: WebServiceInfo by lazy { WebServiceInfoAdapter(host, getMappedPort(servicePort), getMappedPort(healthPort)) }
+class ExampleWriteEndpointServiceContainer(private val servicePort: Int, private val healthPort: Int) : GenericContainer<ExampleWriteEndpointServiceContainer>(imageName) {
+
+    val webServiceInfo by lazy { WebServiceInfo.create(host, getMappedPort(servicePort), getMappedPort(healthPort)) }
+
+    companion object {
+        val imageName: DockerImageName = DockerImageName.parse("$SERVICE_OCI_IMAGE_REPOSITORY$SERVICE_OCI_IMAGE_NAME")
+    }
 }
-
-// TODO refactor & move or delete
-private data class WebServiceInfoAdapter(override val host: String, override val port: Int, override val healthPort: Int) : WebServiceInfo
-
-//fun newExampleWriteEndpointServiceContainer(pulsar: PulsarContainer, topic: PulsarTopic, exposedHttpPort: Int = 8080): ExampleWriteEndpointServiceContainer {
-//
-//    val pulsarBrokerUrl = "pulsar://${pulsar.networkAlias}:${PulsarContainer.BROKER_PORT}"
-//    return newEndpointServiceContainer(pulsarBrokerUrl, topic, exposedHttpPort)
-//}
-//
-//fun newExampleWriteEndpointServiceContainer(pulsarBrokerUrl: String, topic: PulsarTopic, exposedHttpPort: Int = 8080): ExampleWriteEndpointServiceContainer {
-//
-//    val pulsarArgs = mapOf(ApplicationProperties.Pulsar.Producer.brokerUrl to pulsarBrokerUrl, ApplicationProperties.Pulsar.Producer.outboundTopic to topic.fullTopicName)
-//    val loggingArguments = mapOf("LOGGING_LEVEL_DEFAULT" to "INFO", "LOGGING_LEVELS" to "io.micronaut.web.router=INFO")
-//    val webArguments = mapOf(ApplicationProperties.HttpServer.port to "$exposedHttpPort", "LOGGING_LEVELS" to "io.micronaut.web.router=INFO")
-//    val arguments = (webArguments + pulsarArgs + loggingArguments)
-//    return EndpointServiceContainer(exposedHttpPort).withExposedPorts(exposedHttpPort).withJavaArgs(arguments)
-//}
-//
-//class ExampleWriteEndpointServiceContainer(val exposedHttpPort: Int) : MicronautMicroserviceContainer<ExampleWriteEndpointServiceContainer>(imageName) {
-//
-//    val serverInfo by lazy { ServerInformation(URLProtocol.HTTP, host, getMappedPort(exposedHttpPort)) }
-//}
 
 // TODO move
 private fun Map.Entry<String, String>.toJavaArgument(): String = "-D$key=$value"
 
+// TODO move
 fun Map<String, String>.toJavaArgumentsList(): List<String> = map { it.toJavaArgument() }
 
-// TODO maybe it takes a port? but it wouldn't know which port...
-fun readinessHttpWaitStrategy(port: Int, path: String = "readiness"): HttpWaitStrategy = Wait.forHttp(path).forPort(port).withMethod("GET").forStatusCode(200)
-
+// TODO move
 fun <CONTAINER : GenericContainer<CONTAINER>> CONTAINER.withJavaArgs(arguments: List<String>): CONTAINER = withEnv("JAVA_TOOL_OPTIONS", arguments.joinToString(separator = " "))
 
+// TODO move
 fun <CONTAINER : GenericContainer<CONTAINER>> CONTAINER.withJavaArgs(arguments: Map<String, String>): CONTAINER = withJavaArgs(arguments.toJavaArgumentsList())
--- web/service/domain/src/main/kotlin/org/sollecitom/chassis/web/service/domain/WebServiceInfo.kt
+++ web/service/domain/src/main/kotlin/org/sollecitom/chassis/web/service/domain/WebServiceInfo.kt
@@ -3,7 +3,14 @@ package org.sollecitom.chassis.web.service.domain
 // TODO refactor this thing
 interface WebServiceInfo {
 
+    val host: String
     val port: Int
     val healthPort: Int
-    val host: String
+
+    companion object {
+
+        fun create(host: String, port: Int, healthPort: Int): WebServiceInfo = WebServiceInfoData(host, port, healthPort)
+    }
 }
+
+private data class WebServiceInfoData(override val host: String, override val port: Int, override val healthPort: Int) : WebServiceInfo
