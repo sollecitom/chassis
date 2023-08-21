bf3410894b0f72b06e3663014717bf2e5fd5e724
Michele Sollecito <michele.sollecito@gmail.com>
2023-08-21 14:25:08 +0100

    WIP

--- example/service/write-endpoint/adapters/driving/web/src/test/kotlin/org/sollecitom/chassis/example/service/endpoint/write/adapters/driving/web/WebApiContractTests.kt
+++ example/service/write-endpoint/adapters/driving/web/src/test/kotlin/org/sollecitom/chassis/example/service/endpoint/write/adapters/driving/web/WebApiContractTests.kt
@@ -31,6 +31,7 @@ private class WebApiContractTests {
     // TODO add the case where the result is rejected as EmailAddressAlreadyInUse
     // TODO add the swagger compliance checks for requests
     // TODO add the swagger compliance checks for responses
+    // TODO test the declared swagger for compliance to standards (using openapi-checker and the StandardOpenApiRulesSet)
 
     @Test
     fun `submitting a register user command for an unregistered user`() {
