98ef2ba8e8c0c48659927d012fc25987f22027d6
Michele Sollecito <michele.sollecito@gmail.com>
2023-08-25 16:30:20 +0100

    WIP

--- correlation/core/test/utils/src/test/kotlin/org/sollecitom/chassis/correlation/core/test/utils/access/AccessTestFactoryTests.kt
+++ correlation/core/test/utils/src/test/kotlin/org/sollecitom/chassis/correlation/core/test/utils/access/AccessTestFactoryTests.kt
@@ -12,14 +12,17 @@ import org.sollecitom.chassis.core.domain.identity.ulid.ULID
 import org.sollecitom.chassis.core.domain.networking.IpAddress
 import org.sollecitom.chassis.core.test.utils.testProvider
 import org.sollecitom.chassis.core.utils.WithCoreGenerators
-import org.sollecitom.chassis.correlation.core.domain.access.*
+import org.sollecitom.chassis.correlation.core.domain.access.Access
 import org.sollecitom.chassis.correlation.core.domain.access.actor.Actor
 import org.sollecitom.chassis.correlation.core.domain.access.actor.DirectActor
+import org.sollecitom.chassis.correlation.core.domain.access.authenticatedOrFailure
+import org.sollecitom.chassis.correlation.core.domain.access.authenticatedOrThrow
 import org.sollecitom.chassis.correlation.core.domain.access.authentication.Authentication
 import org.sollecitom.chassis.correlation.core.domain.access.authentication.CredentialsBasedAuthentication
 import org.sollecitom.chassis.correlation.core.domain.access.session.SimpleSession
 import org.sollecitom.chassis.correlation.core.domain.origin.Origin
 import org.sollecitom.chassis.correlation.core.domain.tenancy.Tenant
+import org.sollecitom.chassis.correlation.core.test.utils.access.authentication.create
 import org.sollecitom.chassis.correlation.core.test.utils.origin.create
 import org.sollecitom.chassis.correlation.core.test.utils.tenancy.create
 import org.sollecitom.chassis.test.utils.assertions.failedThrowing
@@ -33,9 +36,8 @@ private class AccessExampleTests : WithCoreGenerators by WithCoreGenerators.test
         @Test
         fun `with given actor and origin`() {
 
-            val token = Authentication.Token(id = newId.string(), validFrom = null, validTo = null)
             val session = SimpleSession(id = newId.string())
-            val actor = DirectActor(account = Actor.UserAccount(id = newId.ulid(), tenant = Tenant.create()), CredentialsBasedAuthentication(token, session))
+            val actor = DirectActor(account = Actor.UserAccount(id = newId.ulid(), tenant = Tenant.create()), CredentialsBasedAuthentication(Authentication.Token.create(), session))
             val origin = Origin.create(ipAddress = IpAddress.create("120.19.6.12"))
 
             val access = Access.Authenticated(actor, origin)
