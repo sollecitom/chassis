d5c8d2c78261f1d5df90cef8545fa5abbe696db5
Michele Sollecito <michele.sollecito@gmail.com>
2023-08-25 12:57:22 +0100

    WIP

--- correlation/core/test/utils/src/test/kotlin/org/sollecitom/chassis/correlation/core/test/utils/access/AccessTestFactoryTests.kt
+++ correlation/core/test/utils/src/test/kotlin/org/sollecitom/chassis/correlation/core/test/utils/access/AccessTestFactoryTests.kt
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
+import org.sollecitom.chassis.core.domain.identity.Id
+import org.sollecitom.chassis.core.domain.identity.StringId
 import org.sollecitom.chassis.core.domain.networking.IpAddress
 import org.sollecitom.chassis.core.test.utils.testProvider
 import org.sollecitom.chassis.core.utils.WithCoreGenerators
@@ -17,6 +19,12 @@ import org.sollecitom.chassis.test.utils.assertions.failedThrowing
 
 @TestInstance(PER_CLASS)
 private class AccessExampleTests : WithCoreGenerators by WithCoreGenerators.testProvider {
+
+    @Nested
+    inner class Authenticated {
+
+
+    }
 
     @Nested
     inner class Unauthenticated {
@@ -89,10 +97,34 @@ sealed class Access(val origin: Origin, val isAuthenticated: Boolean) {
         companion object
     }
 
-    sealed class Authenticated(origin: Origin) : Access(origin, true)
+    sealed class Authenticated(origin: Origin) : Access(origin, true) {
+
+        // branch here Direct vs Indirect?
+        // branch heere Simple vs Impersonating vs Acting on Behalf?
+        // should this distinction be at Actor level?
+    }
 
     companion object
 }
+
+// TODO could an access ever be cross tenant? YES! if we impersonate a customer's user, as an example
+sealed class Actor<ID : Id<ID>> {
+
+    // Account?
+    // internal vs external
+    // service vs user
+    // where do we put access mechanism? can we restrict this by using the compiler, so that a service account cannot come with SSO through Google say?
+
+    sealed class Account<ID : Id<ID>>(val id: ID)
+    class UserAccount
+    class ServiceAccount
+}
+
+class DirectActor<ID : Id<ID>>
+
+class ImpersonatingActor<ID : Id<ID>>
+
+class ActorOnBehalf<ID : Id<ID>>
 
 fun Access.authenticatedOrThrow(): Access.Authenticated = authenticatedOrNull() ?: error("Access is unauthenticated")
 
