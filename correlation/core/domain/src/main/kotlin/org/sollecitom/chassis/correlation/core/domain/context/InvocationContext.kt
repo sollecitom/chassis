b328b917d0a50ab62ec5cd4e58adf1eb9db1dcaa
Michele Sollecito <michele.sollecito@gmail.com>
2023-08-26 09:18:50 +0100

    WIP

--- correlation/core/domain/src/main/kotlin/org/sollecitom/chassis/correlation/core/domain/context/InvocationContext.kt
+++ correlation/core/domain/src/main/kotlin/org/sollecitom/chassis/correlation/core/domain/context/InvocationContext.kt
@@ -1,12 +1,10 @@
 package org.sollecitom.chassis.correlation.core.domain.context
 
-import org.sollecitom.chassis.core.domain.identity.Id
 import org.sollecitom.chassis.core.domain.identity.SortableTimestampedUniqueIdentifier
 import org.sollecitom.chassis.correlation.core.domain.access.Access
-import org.sollecitom.chassis.correlation.core.domain.access.authentication.Authentication
 import org.sollecitom.chassis.correlation.core.domain.trace.Trace
 
-data class InvocationContext<ACCESS : Access<ACCESS_ID, AUTHENTICATION>, ACCESS_ID : Id<ACCESS_ID>, AUTHENTICATION : Authentication, TRACE_ID : SortableTimestampedUniqueIdentifier<TRACE_ID>>(val access: ACCESS, val trace: Trace<TRACE_ID>) {
+data class InvocationContext<ACCESS : Access<*, *>, TRACE_ID : SortableTimestampedUniqueIdentifier<TRACE_ID>>(val access: ACCESS, val trace: Trace<TRACE_ID>) {
 
     companion object
 }
