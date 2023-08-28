7b8662cc8a8b9e1917d02c9eb2f031dd95c4f664
Michele Sollecito <michele.sollecito@gmail.com>
2023-08-28 09:22:00 +0100

    WIP

--- example/service/write-endpoint/application/src/test/kotlin/org/sollecitom/chassis/example/service/endpoint/write/application/InMemoryUserRepository.kt
+++ example/service/write-endpoint/application/src/test/kotlin/org/sollecitom/chassis/example/service/endpoint/write/application/InMemoryUserRepository.kt
@@ -1,62 +1,78 @@
 package org.sollecitom.chassis.example.service.endpoint.write.application
 
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.firstOrNull
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
+import kotlinx.coroutines.runBlocking
 import org.sollecitom.chassis.core.domain.email.EmailAddress
-import org.sollecitom.chassis.core.domain.identity.SortableTimestampedUniqueIdentifier
+import org.sollecitom.chassis.core.domain.identity.Id
 import org.sollecitom.chassis.core.utils.WithCoreGenerators
 import org.sollecitom.chassis.ddd.domain.EntityEvent
 import org.sollecitom.chassis.ddd.domain.EntityEventStore
+import org.sollecitom.chassis.ddd.domain.Event
 import org.sollecitom.chassis.ddd.domain.EventStore
-import org.sollecitom.chassis.example.service.endpoint.write.domain.user.User
-import org.sollecitom.chassis.example.service.endpoint.write.domain.user.UserRegistrationRequestWasAlreadySubmitted
-import org.sollecitom.chassis.example.service.endpoint.write.domain.user.UserRegistrationRequestWasSubmitted
-import org.sollecitom.chassis.example.service.endpoint.write.domain.user.UserRepository
+import org.sollecitom.chassis.example.service.endpoint.write.domain.user.*
 
 // TODO remove events from here, and pass a source of events instead
 class InMemoryUserRepository(private val events: EventStore.Mutable, private val coreGenerators: WithCoreGenerators) : UserRepository, WithCoreGenerators by coreGenerators {
 
-    private val userByEmail = mutableMapOf<EmailAddress, User>()
+    override suspend fun withEmailAddress(emailAddress: EmailAddress) = eventSourcedUser(emailAddress)
 
-    override suspend fun withEmailAddress(emailAddress: EmailAddress) = userByEmail.computeIfAbsent(emailAddress, ::createNewUser)
+    private fun eventSourcedUser(emailAddress: EmailAddress): User = runBlocking {
 
-    private fun createNewUser(emailAddress: EmailAddress): User {
-
-        val userId = newId.internal()
-        return EventSourcedUser(_events = events.forEntity(userId), id = userId, emailAddress = emailAddress, coreGenerators = coreGenerators)
+        when (val previousRegistration = previousRegistration(emailAddress, events.history())) {
+            is UserRegistrationRequestWasSubmitted.V1 -> RegisteredUser(previousRegistration, coreGenerators, events.forEntity(previousRegistration.entityId))
+            null -> {
+                val userId = newId.internal()
+                UnregisteredUser(userId, emailAddress, events.forEntity(userId), this@InMemoryUserRepository)
+            }
+        }
     }
 
-    private class EventSourcedUser(override val id: SortableTimestampedUniqueIdentifier<*>, private val emailAddress: EmailAddress, private val _events: EntityEventStore.Mutable, coreGenerators: WithCoreGenerators) : User, WithCoreGenerators by coreGenerators {
+    private suspend fun previousRegistration(emailAddress: EmailAddress, history: Flow<Event>): UserRegistrationRequestWasSubmitted? {
 
-        private val history get() = _events.history()
+        return history.firstOrNull { it is UserRegistrationRequestWasSubmitted && it.emailAddress == emailAddress }?.let { it as UserRegistrationRequestWasSubmitted }
+    }
+
+    private class RegisteredUser(private val pastEvent: UserRegistrationRequestWasSubmitted, coreGenerators: WithCoreGenerators, private val _events: EntityEventStore.Mutable) : User, WithCoreGenerators by coreGenerators {
+
         override val events: EntityEventStore get() = _events
-        private val mutex = Mutex()
+        override val id: Id get() = pastEvent.userId
 
         init {
             require(events.entityId == id) { "The entity ID for the entity-specific event store '${events.entityId}' doesn't match the entity ID of the user '$id'" }
         }
 
-        // TODO refactor this and introduce the state pattern
-        override suspend fun submitRegistrationRequest() = mutex.withLock {
+        override suspend fun submitRegistrationRequest(): UserRegistrationRequestWasAlreadySubmitted.V1 {
 
-            previousRegistration()?.let { return@withLock it.alreadySubmitted() }
-            val event = registrationRequestWasSubmitted()
-            publish(event) // TODO remove this
-            event
+            val event = pastEvent.alreadySubmitted()
+            publish(event)
+            return event
         }
 
         private fun UserRegistrationRequestWasSubmitted.alreadySubmitted() = UserRegistrationRequestWasAlreadySubmitted.V1(emailAddress = emailAddress, userId = userId, id = newId.internal(), timestamp = clock.now())
+
+        private suspend fun publish(event: EntityEvent) = _events.publish(event)
+    }
+
+    private class UnregisteredUser(override val id: Id, private val emailAddress: EmailAddress, private val _events: EntityEventStore.Mutable, coreGenerators: WithCoreGenerators) : User, WithCoreGenerators by coreGenerators {
+
+        init {
+            require(events.entityId == id) { "The entity ID for the entity-specific event store '${events.entityId}' doesn't match the entity ID of the user '$id'" }
+        }
+
+        override val events: EntityEventStore get() = _events
+
+        override suspend fun submitRegistrationRequest(): UserRegistrationEvent {
+
+            val event = registrationRequestWasSubmitted()
+            publish(event)
+            return event
+        }
 
         private fun registrationRequestWasSubmitted(): UserRegistrationRequestWasSubmitted.V1 {
 
             val now = clock.now()
             return UserRegistrationRequestWasSubmitted.V1(emailAddress = emailAddress, userId = id, id = newId.internal(now), timestamp = now)
-        }
-
-        private suspend fun previousRegistration(): UserRegistrationRequestWasSubmitted? {
-
-            return history.firstOrNull { it is UserRegistrationRequestWasSubmitted && it.emailAddress == emailAddress }?.let { it as UserRegistrationRequestWasSubmitted }
         }
 
         private suspend fun publish(event: EntityEvent) = _events.publish(event)
