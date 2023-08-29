6ef8cebc20e637b5a3821f7f690f76858bd49a9f
Michele Sollecito <michele.sollecito@gmail.com>
2023-08-29 16:40:32 +0100

    WIP

--- correlation/core/serialization/json/src/test/kotlin/org/sollecitom/chassis/correlation/core/serialization/json/access/actor/ActorJsonSerializationTests.kt
+++ correlation/core/serialization/json/src/test/kotlin/org/sollecitom/chassis/correlation/core/serialization/json/access/actor/ActorJsonSerializationTests.kt
@@ -1,11 +1,7 @@
 package org.sollecitom.chassis.correlation.core.serialization.json.access.actor
 
-import assertk.assertThat
-import assertk.assertions.isEqualTo
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.MethodSource
 import org.sollecitom.chassis.core.test.utils.testProvider
 import org.sollecitom.chassis.core.utils.WithCoreGenerators
 import org.sollecitom.chassis.correlation.core.domain.access.actor.Actor
@@ -14,7 +10,6 @@ import org.sollecitom.chassis.correlation.core.domain.access.actor.onBehalfOf
 import org.sollecitom.chassis.correlation.core.test.utils.access.actor.create
 import org.sollecitom.chassis.correlation.core.test.utils.access.actor.direct
 import org.sollecitom.chassis.json.test.utils.JsonSerdeTestSpecification
-import org.sollecitom.chassis.json.test.utils.compliesWith
 import org.sollecitom.chassis.test.utils.params.ParameterizedTestSupport
 
 @TestInstance(PER_CLASS)
@@ -23,21 +18,8 @@ private class ActorJsonSerializationTests : JsonSerdeTestSpecification<Actor>, W
     override val jsonSerde get() = Actor.jsonSerde
 
     override fun arguments() = ParameterizedTestSupport.arguments(
-//        "direct" to Actor.direct(),
-//        "on-behalf" to Actor.direct().onBehalfOf(Actor.UserAccount.create()),
+        "direct" to Actor.direct(),
+        "on-behalf" to Actor.direct().onBehalfOf(Actor.UserAccount.create()),
         "impersonating" to Actor.direct().impersonating(Actor.UserAccount.create()),
     )
-
-    // TODO fix and remove!
-    @ParameterizedTest
-    @MethodSource("arguments")
-    override fun `serializing and deserializing to and from JSON`(argument: ParameterizedTestSupport.InlineWrapper<Actor>) {
-
-        val value = argument.value
-        val json = jsonSerde.serialize(value)
-        val deserialized = jsonSerde.deserialize(json)
-
-        assertThat(deserialized).isEqualTo(value)
-        assertThat(json).compliesWith(jsonSerde.schema)
-    }
 }
