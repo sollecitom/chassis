51d122c139f7eb9134351e552edc5ebc56115a4f
Michele Sollecito <michele.sollecito@gmail.com>
2023-08-29 13:14:34 +0100

    WIP

---
+++ correlation/core/serialization/json/src/main/kotlin/org/sollecitom/chassis/correlation/core/serialization/json/access/autorization/AuthenticationPrincipalJsonSerde.kt
@@ -0,0 +1,31 @@
+package org.sollecitom.chassis.correlation.core.serialization.json.access.autorization
+
+import com.github.erosb.jsonsKema.Schema
+import org.json.JSONObject
+import org.sollecitom.chassis.correlation.core.domain.access.authorization.AuthorizationPrincipal
+import org.sollecitom.chassis.correlation.core.domain.access.authorization.Roles
+import org.sollecitom.chassis.json.utils.getRequiredJSONObject
+import org.sollecitom.chassis.json.utils.jsonSchemaAt
+import org.sollecitom.chassis.json.utils.serde.JsonSerde
+
+private object AuthenticationPrincipalJsonSerde : JsonSerde.SchemaAware<AuthorizationPrincipal> {
+
+    override val schema: Schema by lazy { jsonSchemaAt("access/authorization/AuthorizationPrincipal.json") }
+
+    override fun serialize(value: AuthorizationPrincipal) = JSONObject().apply {
+
+        put(Fields.ROLES, Roles.jsonSerde.serialize(value.roles))
+    }
+
+    override fun deserialize(json: JSONObject): AuthorizationPrincipal {
+
+        val roles = json.getRequiredJSONObject(Fields.ROLES).let(Roles.jsonSerde::deserialize)
+        return AuthorizationPrincipal(roles = roles)
+    }
+
+    private object Fields {
+        const val ROLES = "roles"
+    }
+}
+
+val AuthorizationPrincipal.Companion.jsonSerde: JsonSerde.SchemaAware<AuthorizationPrincipal> get() = AuthenticationPrincipalJsonSerde
---
+++ correlation/core/serialization/json/src/test/kotlin/org/sollecitom/chassis/correlation/core/serialization/json/access/authorization/AuthorizationPrincipalJsonSerializationTests.kt
@@ -0,0 +1,18 @@
+package org.sollecitom.chassis.correlation.core.serialization.json.access.authorization
+
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
+import org.sollecitom.chassis.core.test.utils.testProvider
+import org.sollecitom.chassis.core.utils.WithCoreGenerators
+import org.sollecitom.chassis.correlation.core.domain.access.authorization.AuthorizationPrincipal
+import org.sollecitom.chassis.correlation.core.serialization.json.access.autorization.jsonSerde
+import org.sollecitom.chassis.correlation.core.test.utils.access.authorization.create
+import org.sollecitom.chassis.json.test.utils.JsonSerdeTestSpecification
+
+@TestInstance(PER_CLASS)
+private class AuthorizationPrincipalJsonSerializationTests : JsonSerdeTestSpecification<AuthorizationPrincipal>, WithCoreGenerators by WithCoreGenerators.testProvider {
+
+    override fun value() = AuthorizationPrincipal.create()
+
+    override val jsonSerde get() = AuthorizationPrincipal.jsonSerde
+}
