b2b3ead7c0c09c2d0b934f6cba0093104ba1b1ac
Michele Sollecito <michele.sollecito@gmail.com>
2023-09-02 06:38:54 +0100

    WIP

--- example/service/write-endpoint/adapters/driving/web/src/main/kotlin/org/sollecitom/chassis/example/service/endpoint/write/adapters/driving/web/api/serde/UserWithPendingRegistrationJsonSerde.kt
+++ example/service/write-endpoint/adapters/driving/web/src/main/kotlin/org/sollecitom/chassis/example/service/endpoint/write/adapters/driving/web/api/serde/UserWithPendingRegistrationJsonSerde.kt
@@ -1,20 +1,24 @@
 package org.sollecitom.chassis.example.service.endpoint.write.adapters.driving.web.api.serde
 
 import org.json.JSONObject
-import org.sollecitom.chassis.core.domain.identity.ULID
+import org.sollecitom.chassis.core.domain.identity.Id
+import org.sollecitom.chassis.correlation.core.serialization.json.identity.jsonSerde
 import org.sollecitom.chassis.example.service.endpoint.write.application.user.UserWithPendingRegistration
-import org.sollecitom.chassis.json.utils.getRequiredString
 import org.sollecitom.chassis.json.utils.serde.JsonSerde
+import org.sollecitom.chassis.json.utils.serde.getValue
+import org.sollecitom.chassis.json.utils.serde.setValue
 
 val UserWithPendingRegistration.Companion.serde: JsonSerde<UserWithPendingRegistration> get() = UserWithPendingRegistrationJsonSerde
 
 private object UserWithPendingRegistrationJsonSerde : JsonSerde<UserWithPendingRegistration> {
 
-    override fun serialize(value: UserWithPendingRegistration): JSONObject = JSONObject().put(Fields.ID, value.id.stringValue)
+    override fun serialize(value: UserWithPendingRegistration) = JSONObject().apply {
+        setValue(Fields.ID, value.id, Id.jsonSerde)
+    }
 
     override fun deserialize(json: JSONObject): UserWithPendingRegistration {
 
-        val id = json.getRequiredString(Fields.ID).let(ULID::invoke)
+        val id = json.getValue(Fields.ID, Id.jsonSerde)
         return UserWithPendingRegistration(id)
     }
 
