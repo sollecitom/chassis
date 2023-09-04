26b88cc9562d87e2bad982861821053b09d087b8
Michele Sollecito <michele.sollecito@gmail.com>
2023-09-04 16:02:28 +0100

    WIP

---
+++ correlation/core/test/utils/src/test/kotlin/org/sollecitom/chassis/correlation/core/test/utils/toggles/TogglesTestFactoryTests.kt
@@ -0,0 +1,30 @@
+package org.sollecitom.chassis.correlation.core.test.utils.toggles
+
+import assertk.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
+import org.sollecitom.chassis.core.test.utils.testProvider
+import org.sollecitom.chassis.core.utils.WithCoreGenerators
+import org.sollecitom.chassis.correlation.core.domain.toggles.BooleanToggleValue
+import org.sollecitom.chassis.correlation.core.domain.toggles.DecimalToggleValue
+import org.sollecitom.chassis.correlation.core.domain.toggles.EnumToggleValue
+import org.sollecitom.chassis.correlation.core.domain.toggles.Toggles
+import org.sollecitom.chassis.test.utils.assertions.containsSameElementsAs
+
+@TestInstance(PER_CLASS)
+private class TogglesTestFactoryTests : WithCoreGenerators by WithCoreGenerators.testProvider {
+
+    @Test
+    fun `with given values`() {
+
+        val toggle1 = BooleanToggleValue(id = newId.internal(), value = true)
+        val toggle2 = DecimalToggleValue(id = newId.internal(), value = 12.5)
+        val toggle3 = EnumToggleValue(id = newId.internal(), value = "HIGH")
+        val values = setOf(toggle1, toggle2, toggle3)
+
+        val toggles = Toggles.create(values = values)
+
+        assertThat(toggles.values).containsSameElementsAs(values)
+    }
+}
