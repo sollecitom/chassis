a76993a5e438e6a7b37d7fc7d923b1534c464eef
Michele Sollecito <michele.sollecito@gmail.com>
2023-09-06 15:13:07 +0100

    WIP

--- example/service/write-endpoint/configuration/src/main/kotlin/org/sollecitom/chassis/example/service/endpoint/write/configuration/LoggingConfiguration.kt
+++ example/service/write-endpoint/configuration/src/main/kotlin/org/sollecitom/chassis/example/service/endpoint/write/configuration/LoggingConfiguration.kt
@@ -7,19 +7,16 @@ import org.sollecitom.chassis.logger.core.LoggingLevel
 import org.sollecitom.chassis.logging.standard.configuration.StandardLoggingConfiguration
 import org.sollecitom.chassis.logging.standard.configuration.applyTo
 
-// TODO move
 fun configureLogging(defaultMinimumLoggingLevel: LoggingLevel = LoggingLevel.INFO) {
 
-    StandardLoggingConfiguration(defaultMinimumLoggingLevel = defaultMinimumLoggingLevel, defaultMinimumLoggingLevelOverrides = minimumLoggingLevelOverrides()).applyTo(JvmLoggerFactory)
+    StandardLoggingConfiguration(defaultMinimumLoggingLevel = defaultMinimumLoggingLevel, defaultMinimumLoggingLevelOverrides = minimumLoggingLevelOverrides).applyTo(JvmLoggerFactory)
 }
 
-// TODO move
 fun configureLogging(environment: Environment) {
 
-    StandardLoggingConfiguration(environment = environment, defaultMinimumLoggingLevelOverrides = minimumLoggingLevelOverrides()).applyTo(JvmLoggerFactory)
+    StandardLoggingConfiguration(environment = environment, defaultMinimumLoggingLevelOverrides = minimumLoggingLevelOverrides).applyTo(JvmLoggerFactory)
 }
 
-// TODO move
 fun configureLogging(log: Log) = JvmLoggerFactory.configure { loggingFunction = log }
 
-private fun minimumLoggingLevelOverrides() = mapOf("org.eclipse.jetty" to LoggingLevel.WARN, "org.apache.hc" to LoggingLevel.WARN)
+private val minimumLoggingLevelOverrides = mapOf("org.eclipse.jetty" to LoggingLevel.WARN, "org.apache.hc" to LoggingLevel.WARN)
