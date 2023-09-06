83f02d761c2b5a218d7340dceeff89d6c4e43f2c
Michele Sollecito <michele.sollecito@gmail.com>
2023-09-06 16:00:17 +0100

    WIP

--- openapi/checking/checker/src/main/kotlin/org/sollecitom/chassis/openapi/checking/checker/sets/StandardTracingHeadersOpenApiRules.kt
+++ openapi/checking/checker/src/main/kotlin/org/sollecitom/chassis/openapi/checking/checker/sets/StandardTracingHeadersOpenApiRules.kt
@@ -2,10 +2,9 @@ package org.sollecitom.chassis.openapi.checking.checker.sets
 
 import org.sollecitom.chassis.openapi.checking.checker.rule.OpenApiRule
 
-// TODO populate
 object StandardTracingHeadersOpenApiRules : OpenApiRuleSet {
 
-    override val rules: Set<OpenApiRule> by lazy {
+    override val rules: Set<OpenApiRule> by lazy { // put some rules here
         setOf(
 
         )
