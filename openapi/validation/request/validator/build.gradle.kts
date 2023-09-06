ead237e6f57f94bfa906ebb2190cac6965c98ed5
Michele Sollecito <michele.sollecito@gmail.com>
2023-09-06 14:12:16 +0100

    WIP

--- openapi/validation/request/validator/build.gradle.kts
+++ openapi/validation/request/validator/build.gradle.kts
@@ -1,6 +1,8 @@
 dependencies {
-    api(projects.chassisOpenapiParser) // TODO should this be hidden as implementation instead?
-    api(libs.swagger.request.validator) // TODO should this be hidden as implementation instead?
+    api(projects.chassisOpenapiParser)
+    api(libs.swagger.request.validator) {
+        exclude(group = "commons-codec", module = "commons-codec")
+    }
 
     implementation(projects.chassisKotlinExtensions)
 }
