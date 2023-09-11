6b3e3dcb3c7bc214d1f61de9f8e940d7f4c2db38
Michele Sollecito <michele.sollecito@gmail.com>
2023-09-11 19:43:12 +0100

    WIP

--- pulsar/test/utils/src/main/kotlin/org/sollecitom/chassis/pulsar/test/utils/PulsarContainerExtensions.kt
+++ pulsar/test/utils/src/main/kotlin/org/sollecitom/chassis/pulsar/test/utils/PulsarContainerExtensions.kt
@@ -1,14 +1,12 @@
 package org.sollecitom.chassis.pulsar.test.utils
 
-import com.github.dockerjava.zerodep.shaded.org.apache.hc.core5.http.HttpStatus
 import org.apache.pulsar.client.admin.PulsarAdmin
 import org.apache.pulsar.client.admin.PulsarAdminBuilder
 import org.apache.pulsar.client.api.ClientBuilder
 import org.apache.pulsar.client.api.PulsarClient
 import org.testcontainers.containers.Network
 import org.testcontainers.containers.PulsarContainer
-import org.testcontainers.containers.wait.strategy.HttpWaitStrategy
-import org.testcontainers.containers.wait.strategy.Wait
+import org.testcontainers.containers.wait.strategy.WaitStrategy
 import org.testcontainers.utility.DockerImageName
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.minutes
@@ -19,23 +17,19 @@ private const val PULSAR_NETWORK_ALIAS = "pulsar"
 
 private fun pulsarDockerImageName(version: String) = DockerImageName.parse("apachepulsar/pulsar:$version")
 
-fun newPulsarContainer(version: String = DEFAULT_PULSAR_DOCKER_IMAGE_VERSION, startupAttempts: Int = 10, startupTimeout: Duration = 2.minutes): PulsarContainer {
+fun newPulsarContainer(version: String = DEFAULT_PULSAR_DOCKER_IMAGE_VERSION, startupAttempts: Int = 10, startupTimeout: Duration = 2.minutes, waitStrategy: WaitStrategy = PulsarWaitStrategies.availableAdminClusterHttpEndpoint): PulsarContainer {
 
-    return PulsarContainer(pulsarDockerImageName(version))
-        .withStartupAttempts(startupAttempts)
-        .withStartupTimeout(startupTimeout.toJavaDuration())
-        .apply { setWaitStrategy(PulsarWaitStrategies.adminClusterHttpEndpoint) }
+    return PulsarContainer(pulsarDockerImageName(version)).withStartupAttempts(startupAttempts).withStartupTimeout(startupTimeout.toJavaDuration()).apply { setWaitStrategy(waitStrategy) }
+}
+
+fun PulsarContainer.withBrokerEnv(propertyName: String, propertyValue: String) = withEnv("PULSAR_PREFIX_$propertyName", propertyValue);
+
+fun PulsarContainer.withBrokerEnv(variables: Map<String, String>): PulsarContainer {
+
+    return variables.entries.map(Map.Entry<String, String>::toPair).fold(this) { container, (propertyName, propertyValue) -> container.withEnv(propertyName, propertyValue) }
 }
 
 fun PulsarContainer.withNetworkAndAliases(network: Network, vararg aliases: String = arrayOf(PULSAR_NETWORK_ALIAS)) = withNetwork(network).withNetworkAliases(*aliases)
-
-object PulsarWaitStrategies {
-
-    private const val BROKER_HTTP_PORT = 8080
-    private const val ADMIN_CLUSTERS_ENDPOINT = "/admin/v2/clusters"
-    private const val EXPECTED_RESPONSE_WHEN_READY = "[\"standalone\"]"
-    val adminClusterHttpEndpoint: HttpWaitStrategy = Wait.forHttp(ADMIN_CLUSTERS_ENDPOINT).forPort(BROKER_HTTP_PORT).forStatusCode(HttpStatus.SC_OK).forResponsePredicate { it == EXPECTED_RESPONSE_WHEN_READY }
-}
 
 fun PulsarContainer.client(customise: ClientBuilder.() -> Unit = {}): PulsarClient = PulsarClient.builder().serviceUrl(pulsarBrokerUrl).also(customise).build()
 
