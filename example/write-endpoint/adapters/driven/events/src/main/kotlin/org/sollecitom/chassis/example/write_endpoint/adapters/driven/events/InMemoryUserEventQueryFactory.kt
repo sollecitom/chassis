8a4e647640e2759f115e419e5e40e6c06e46bd2f
Michele Sollecito <michele.sollecito@gmail.com>
2023-09-14 11:32:39 +0100

    WIP

--- example/write-endpoint/adapters/driven/events/src/main/kotlin/org/sollecitom/chassis/example/write_endpoint/adapters/driven/events/InMemoryUserEventQueryFactory.kt
+++ example/write-endpoint/adapters/driven/events/src/main/kotlin/org/sollecitom/chassis/example/write_endpoint/adapters/driven/events/InMemoryUserEventQueryFactory.kt
@@ -10,7 +10,7 @@ import org.sollecitom.chassis.example.write_endpoint.domain.user.UserRegistratio
 import kotlin.reflect.KClass
 
 // TODO create memory and pulsar adapters, with a query factory each
-// TODO move to a separate module? It'd allow to remove the dependency on the in-memory event-store from this module
+// TODO rename this to driven/events/memory
 
 val UserEvent.Companion.inMemoryQueryFactory: InMemoryEventStore.Query.Factory get() = InMemoryUserEventQueryFactory
 
