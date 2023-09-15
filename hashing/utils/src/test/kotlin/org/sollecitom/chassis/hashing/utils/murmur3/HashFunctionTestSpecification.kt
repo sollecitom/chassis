5fc77acf679438b61f731e8860438b32dbf617a8
Michele Sollecito <michele.sollecito@gmail.com>
2023-09-15 13:27:53 +0100

    WIP

--- hashing/utils/src/test/kotlin/org/sollecitom/chassis/hashing/utils/murmur3/HashFunctionTestSpecification.kt
+++ hashing/utils/src/test/kotlin/org/sollecitom/chassis/hashing/utils/murmur3/HashFunctionTestSpecification.kt
@@ -1,11 +1,20 @@
 package org.sollecitom.chassis.hashing.utils.murmur3
 
+import assertk.Assert
 import assertk.assertThat
+import assertk.assertions.isEqualTo
 import org.junit.jupiter.api.Test
 import org.sollecitom.chassis.kotlin.extensions.text.strings
 import kotlin.random.Random
 
-interface HashFunctionTestSpecification<RESULT : Any> : TestWithMurmur3HashFunction<RESULT> {
+interface HashFunctionTestSpecification<RESULT : Any> {
+
+    val digest: String
+    val expectedHash: RESULT
+
+    val hash: Murmur3HashFunction<RESULT>
+
+    fun Assert<RESULT>.matches(expected: RESULT) = isEqualTo(expected)
 
     @Test
     fun `hashing the same value with the same seed produces the same hash`() {
@@ -20,5 +29,15 @@ interface HashFunctionTestSpecification<RESULT : Any> : TestWithMurmur3HashFunct
 
             assertThat(hash).matches(secondHash)
         }
+    }
+
+    @Test
+    fun `the hash produced matches the one Murmur3 produces`() {
+
+        val bytes = digest.toByteArray()
+
+        val hash = hash(bytes)
+
+        assertThat(hash).matches(expectedHash)
     }
 }
