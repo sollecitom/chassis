c35f5a3bf9dce3f6255e781c4e0f9b2f7d88341a
Michele Sollecito <michele.sollecito@gmail.com>
2023-09-20 16:09:38 +0100

    WIP

--- avro/serialization/utils/src/main/kotlin/org/sollecitom/chassis/avro/serialization/utils/EnvelopeFields.kt
+++ avro/serialization/utils/src/main/kotlin/org/sollecitom/chassis/avro/serialization/utils/EnvelopeFields.kt
@@ -3,5 +3,5 @@ package org.sollecitom.chassis.avro.serialization.utils
 internal object EnvelopeFields {
 
     const val ENVELOPE = "envelope"
-    const val ENVELOPE_TYPE = "envelopeType" // TODO use snake case?
+    const val ENVELOPE_TYPE = "envelope-type"
 }
