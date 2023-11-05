b6a82d5103cde77b91128bb339f9fa5804d3b599
michele <michele.sollecito@gmail.com>
2023-11-05 09:54:58 +0000

    Working towards http4k utils.

--- buildSrc/src/main/kotlin/Plugins.kt
+++ buildSrc/src/main/kotlin/Plugins.kt
@@ -1,5 +1,6 @@
 import org.gradle.api.JavaVersion
 import org.gradle.api.plugins.JavaPluginExtension
+import org.gradle.jvm.toolchain.JavaLanguageVersion
 
 object Plugins {
 
@@ -9,6 +10,9 @@ object Plugins {
             with(plugin) {
                 sourceCompatibility = JavaVersion.VERSION_19
                 targetCompatibility = JavaVersion.VERSION_19
+                toolchain {
+                    languageVersion.set(JavaLanguageVersion.of(JavaVersion.VERSION_19.majorVersion))
+                }
                 withJavadocJar()
                 withSourcesJar()
             }
