0ab885dbb1905612e94e1cee476d37882ca5a111
Michele Sollecito <michele.sollecito@gmail.com>
2023-11-27 12:35:45 +0000

    WIP

--- core/domain/src/main/kotlin/org/sollecitom/chassis/core/domain/currency/CurrencyAmountExtensions.kt
+++ core/domain/src/main/kotlin/org/sollecitom/chassis/core/domain/currency/CurrencyAmountExtensions.kt
@@ -4,19 +4,19 @@ import org.sollecitom.chassis.kotlin.extensions.number.withPrecision
 import java.math.BigDecimal
 
 @Suppress("UNCHECKED_CAST")
-fun <CURRENCY_AMOUNT : CurrencyAmount> CURRENCY_AMOUNT.times(value: Long): CURRENCY_AMOUNT = times(value.toBigInteger()) as CURRENCY_AMOUNT
+operator fun <CURRENCY_AMOUNT : CurrencyAmount> CURRENCY_AMOUNT.times(value: Long): CURRENCY_AMOUNT = times(value.toBigInteger()) as CURRENCY_AMOUNT
 
 @Suppress("UNCHECKED_CAST")
-fun <CURRENCY_AMOUNT : CurrencyAmount> CURRENCY_AMOUNT.div(value: Long): CURRENCY_AMOUNT = div(value.toBigInteger()) as CURRENCY_AMOUNT
+operator fun <CURRENCY_AMOUNT : CurrencyAmount> CURRENCY_AMOUNT.div(value: Long): CURRENCY_AMOUNT = div(value.toBigInteger()) as CURRENCY_AMOUNT
 
 @Suppress("UNCHECKED_CAST")
 fun <CURRENCY_AMOUNT : CurrencyAmount> CURRENCY_AMOUNT.divAndRemainder(value: Long): Pair<CURRENCY_AMOUNT, CURRENCY_AMOUNT> = divAndRemainder(value.toBigInteger()) as Pair<CURRENCY_AMOUNT, CURRENCY_AMOUNT>
 
 @Suppress("UNCHECKED_CAST")
-fun <CURRENCY_AMOUNT : CurrencyAmount> CURRENCY_AMOUNT.times(value: Int): CURRENCY_AMOUNT = times(value.toBigInteger()) as CURRENCY_AMOUNT
+operator fun <CURRENCY_AMOUNT : CurrencyAmount> CURRENCY_AMOUNT.times(value: Int): CURRENCY_AMOUNT = times(value.toBigInteger()) as CURRENCY_AMOUNT
 
 @Suppress("UNCHECKED_CAST")
-fun <CURRENCY_AMOUNT : CurrencyAmount> CURRENCY_AMOUNT.div(value: Int): CURRENCY_AMOUNT = div(value.toBigInteger()) as CURRENCY_AMOUNT
+operator fun <CURRENCY_AMOUNT : CurrencyAmount> CURRENCY_AMOUNT.div(value: Int): CURRENCY_AMOUNT = div(value.toBigInteger()) as CURRENCY_AMOUNT
 
 @Suppress("UNCHECKED_CAST")
 fun <CURRENCY_AMOUNT : CurrencyAmount> CURRENCY_AMOUNT.divAndRemainder(value: Int): Pair<CURRENCY_AMOUNT, CURRENCY_AMOUNT> = divAndRemainder(value.toBigInteger()) as Pair<CURRENCY_AMOUNT, CURRENCY_AMOUNT>
