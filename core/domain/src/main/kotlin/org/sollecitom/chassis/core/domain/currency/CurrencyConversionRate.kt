5ca717ab15562e9c008c0a47d674f271cd208f8b
Michele Sollecito <michele.sollecito@gmail.com>
2023-12-07 15:12:55 +0000

    WIP

--- core/domain/src/main/kotlin/org/sollecitom/chassis/core/domain/currency/CurrencyConversionRate.kt
+++ core/domain/src/main/kotlin/org/sollecitom/chassis/core/domain/currency/CurrencyConversionRate.kt
@@ -5,5 +5,9 @@ import java.math.BigDecimal
 @JvmInline
 value class CurrencyConversionRate(val value: BigDecimal) : Comparable<CurrencyConversionRate> {
 
+    init {
+        require(value > BigDecimal.ZERO) { "A currency conversion rate should be greater than 0" }
+    }
+
     override fun compareTo(other: CurrencyConversionRate) = value.compareTo(other.value)
 }
