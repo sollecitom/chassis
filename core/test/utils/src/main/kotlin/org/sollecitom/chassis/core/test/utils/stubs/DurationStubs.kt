21fa400c8b856371ecbc4f6ac08f4ddda54f9c98
Michele Sollecito <michele.sollecito@gmail.com>
2023-12-11 16:46:09 +0000

    WIP

---
+++ core/test/utils/src/main/kotlin/org/sollecitom/chassis/core/test/utils/stubs/DurationStubs.kt
@@ -0,0 +1,9 @@
+package org.sollecitom.chassis.core.test.utils.stubs
+
+import kotlinx.datetime.Instant
+import org.sollecitom.chassis.core.utils.CoreDataGenerator
+import kotlin.time.Duration
+
+context(CoreDataGenerator)
+val Duration.ago: Instant
+    get() = clock.now() - this
