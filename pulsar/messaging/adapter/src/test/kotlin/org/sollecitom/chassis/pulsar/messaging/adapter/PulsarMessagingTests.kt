faf21a224df646f6478e6e41217caf2b3facffbb
Michele Sollecito <michele.sollecito@gmail.com>
2023-12-12 09:50:53 +0000

    WIP

--- pulsar/messaging/adapter/src/test/kotlin/org/sollecitom/chassis/pulsar/messaging/adapter/PulsarMessagingTests.kt
+++ pulsar/messaging/adapter/src/test/kotlin/org/sollecitom/chassis/pulsar/messaging/adapter/PulsarMessagingTests.kt
@@ -1,33 +1,52 @@
 package org.sollecitom.chassis.pulsar.messaging.adapter
-// // TODO implement
-//import org.junit.jupiter.api.Test
-//import org.junit.jupiter.api.TestInstance
-//import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
-//import org.sollecitom.chassis.core.test.utils.testProvider
-//import org.sollecitom.chassis.core.utils.CoreDataGenerator
-//import org.sollecitom.chassis.logger.core.LoggingLevel
-//import org.sollecitom.chassis.logging.standard.configuration.configureLogging
-//import org.sollecitom.chassis.pulsar.test.utils.admin
-//import org.sollecitom.chassis.pulsar.test.utils.client
-//import org.sollecitom.chassis.pulsar.test.utils.create
-//import org.sollecitom.chassis.pulsar.test.utils.newPulsarContainer
-//import org.sollecitom.chassis.pulsar.utils.PulsarTopic
-//
-//@TestInstance(PER_CLASS)
-//private class PulsarMessagingTests : CoreDataGenerator by CoreDataGenerator.testProvider {
-//
-//    init {
-//        configureLogging(defaultMinimumLoggingLevel = LoggingLevel.INFO)
-//    }
-//
-//    override val pulsar = newPulsarContainer()
-//    override val pulsarClient by lazy { pulsar.client() }
-//    override val pulsarAdmin by lazy { pulsar.admin() }
-//    override val topic = PulsarTopic.create()
-//
-//    @Test
-//    fun `sending and receiving a single messages`() {
-//
-//
-//    }
-//}
+
+import assertk.assertThat
+import assertk.assertions.isNotNull
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
+import org.sollecitom.chassis.core.test.utils.testProvider
+import org.sollecitom.chassis.core.utils.CoreDataGenerator
+import org.sollecitom.chassis.logger.core.LoggingLevel
+import org.sollecitom.chassis.logging.standard.configuration.configureLogging
+import org.sollecitom.chassis.pulsar.test.utils.admin
+import org.sollecitom.chassis.pulsar.test.utils.client
+import org.sollecitom.chassis.pulsar.test.utils.create
+import org.sollecitom.chassis.pulsar.test.utils.newPulsarContainer
+import org.sollecitom.chassis.pulsar.utils.PulsarTopic
+import org.sollecitom.chassis.pulsar.utils.ensureTopicExists
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
+
+@TestInstance(PER_CLASS)
+private class PulsarMessagingTests : CoreDataGenerator by CoreDataGenerator.testProvider {
+
+    init {
+        configureLogging(defaultMinimumLoggingLevel = LoggingLevel.INFO)
+    }
+
+    private val pulsar = newPulsarContainer()
+    private val pulsarClient by lazy { pulsar.client() }
+    private val pulsarAdmin by lazy { pulsar.admin() }
+    private val topic = PulsarTopic.create()
+    private val timeout: Duration get() = 30.seconds
+
+    @BeforeAll
+    fun beforeAll() {
+        pulsar.start()
+        pulsarAdmin.ensureTopicExists(topic = topic, isAllowAutoUpdateSchema = true)
+    }
+
+    @AfterAll
+    fun afterAll() {
+        pulsar.stop()
+    }
+
+    @Test
+    fun `sending and receiving a single message`() {
+
+        assertThat(pulsar.pulsarBrokerUrl).isNotNull()
+    }
+}
