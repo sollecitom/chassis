f6892856fac1df4207e0ace0a8ba6e5de896406d
Michele Sollecito <michele.sollecito@gmail.com>
2023-12-22 11:07:53 +0100

    WIP

--- core/utils/src/main/kotlin/org/sollecitom/chassis/core/utils/CoreDataGenerator.kt
+++ core/utils/src/main/kotlin/org/sollecitom/chassis/core/utils/CoreDataGenerator.kt
@@ -4,6 +4,7 @@ import kotlinx.datetime.Clock
 import org.sollecitom.chassis.core.domain.identity.factory.UniqueIdFactory
 import kotlin.random.Random
 
+// TODO split into 3 traits
 interface CoreDataGenerator {
 
     val random: Random
