a1ecbf45344009b9e5f3fa836f80d41fdf812d72
Michele Sollecito <michele.sollecito@gmail.com>
2023-12-23 13:14:32 +0100

    WIP

--- pulsar/messaging/event/framework/materialised-view/src/test/kotlin/org/sollecitom/chassis/pulsar/messaging/event/framework/materialised/view/PulsarMaterialisedViewEventFrameworkTests.kt
+++ pulsar/messaging/event/framework/materialised-view/src/test/kotlin/org/sollecitom/chassis/pulsar/messaging/event/framework/materialised/view/PulsarMaterialisedViewEventFrameworkTests.kt
@@ -7,8 +7,6 @@ import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
-import org.sollecitom.chassis.core.domain.identity.Id
-import org.sollecitom.chassis.core.domain.identity.StringId
 import org.sollecitom.chassis.core.domain.naming.Name
 import org.sollecitom.chassis.core.test.utils.testProvider
 import org.sollecitom.chassis.core.utils.CoreDataGenerator
@@ -17,7 +15,8 @@ import org.sollecitom.chassis.ddd.domain.store.EventStore
 import org.sollecitom.chassis.ddd.event.framework.test.specification.EventFrameworkTestSpecification
 import org.sollecitom.chassis.ddd.event.store.memory.InMemoryEventStore
 import org.sollecitom.chassis.ddd.stubs.serialization.json.event.testStubJsonSerde
-import org.sollecitom.chassis.json.utils.serde.JsonSerde
+import org.sollecitom.chassis.ddd.test.stubs.TestEntityEvent
+import org.sollecitom.chassis.ddd.test.stubs.TestEvent
 import org.sollecitom.chassis.messaging.domain.Message
 import org.sollecitom.chassis.messaging.domain.OutboundMessage
 import org.sollecitom.chassis.messaging.domain.Topic
@@ -36,40 +35,21 @@ private class PulsarMaterialisedViewEventFrameworkTests : EventFrameworkTestSpec
     override val timeout = 20.seconds
     private val pulsar = newPulsarContainer()
     private val pulsarAdmin by lazy { pulsar.admin() }
-    private val streamName = "test-pulsar-event-stream".let(::Name)
-    private val instanceId = StringId("1")
-    private val eventSerde: JsonSerde.SchemaAware<Event> = Event.testStubJsonSerde
+    private val instanceInfo = InstanceInfo("1".let(::Name), "test-pulsar-event-stream".let(::Name))
     private val instances = mutableListOf<MaterialisedEventFramework>()
 
     context(CoroutineScope)
     override fun candidate(): MaterialisedEventFramework {
 
         val topic = Topic.create()
+        val stream = TestJsonPulsarEventStream(topic)
         val store = InMemoryEventStore()
-        val framework = pulsar.client().pulsarMaterialisedEventFramework(topic, store, streamName, instanceId, eventSerde) { event ->
-
-            OutboundMessage(event.id.stringValue, event, emptyMap(), Message.Context()) // TODO take from a MessageConverter
-        }
+        val framework = pulsar.client().pulsarMaterialisedEventFramework(instanceInfo, stream, store)
         pulsarAdmin.ensureTopicExists(topic = topic, numberOfPartitions = 1, isAllowAutoUpdateSchema = true)
         framework.startBlocking()
         instances += framework
         return framework
     }
-
-    fun PulsarClient.pulsarMaterialisedEventFramework(topic: Topic, store: EventStore.Mutable, streamName: Name, instanceId: Id, serde: JsonSerde.SchemaAware<Event>, eventToMessage: (Event) -> Message<Event>): MaterialisedEventFramework = pulsarMaterialisedEventFramework(instanceId.stringValue, streamName.value, serde, topic, store, eventToMessage)
-
-    // TODO move to main source
-    fun PulsarClient.pulsarMaterialisedEventFramework(instanceId: String, streamName: String, serde: JsonSerde.SchemaAware<Event>, topic: Topic, store: EventStore.Mutable, eventToMessage: (Event) -> Message<Event>): MaterialisedEventFramework {
-
-        val schema = serde.asPulsarSchema()
-        val producer = messageProducer(instanceId, streamName, schema, topic)
-        val consumer = messageConsumer(instanceId, streamName, schema, topic)
-        return MaterialisedEventFramework(topic, store, producer, consumer, eventToMessage)
-    }
-
-    private fun PulsarClient.messageProducer(instanceId: String, streamName: String, schema: Schema<Event>, topic: Topic) = pulsarMessageProducer(topic) { newProducer(schema).topic(it).producerName("$streamName-producer-${instanceId}").create() }
-
-    private fun PulsarClient.messageConsumer(instanceId: String, streamName: String, schema: Schema<Event>, topic: Topic) = pulsarMessageConsumer(topic) { newConsumer(schema).topics(it).consumerName("$streamName-consumer-${instanceId}").subscriptionName(streamName).subscribe() }
 
     @BeforeAll
     fun beforeAll() {
@@ -84,3 +64,53 @@ private class PulsarMaterialisedViewEventFrameworkTests : EventFrameworkTestSpec
         pulsar.stop()
     }
 }
+
+// TODO move to main source
+fun PulsarClient.pulsarMaterialisedEventFramework(instanceInfo: InstanceInfo, stream: PulsarEventStream, store: EventStore.Mutable): MaterialisedEventFramework {
+
+    val producer = messageProducer(instanceInfo, stream)
+    val consumer = messageConsumer(instanceInfo, stream)
+    return MaterialisedEventFramework(stream.topic, store, producer, consumer) { event ->
+
+        OutboundMessage(stream.messageKeyForEvent(event), event, stream.messagePropertiesForEvent(event), Message.Context())
+    }
+}
+
+private fun PulsarClient.messageProducer(instanceInfo: InstanceInfo, stream: PulsarEventStream) = pulsarMessageProducer(stream.topic) { newProducer(stream.schema).topic(it).producerName("${instanceInfo.groupName.value}-producer-${instanceInfo.id.value}").create() }
+
+private fun PulsarClient.messageConsumer(instanceInfo: InstanceInfo, stream: PulsarEventStream) = pulsarMessageConsumer(stream.topic) { newConsumer(stream.schema).topics(it).consumerName("${instanceInfo.groupName.value}-consumer-${instanceInfo.id.value}").subscriptionName(instanceInfo.groupName.value).subscribe() }
+
+// TODO move
+interface PulsarEventStream : EventStream {
+
+    // TODO consider making it generic to the base event type e.g., GenericTestEvent in the stub
+    val schema: Schema<Event>
+}
+
+private class TestJsonPulsarEventStream(topic: Topic) : PulsarEventStream, EventStream by TestEventStream(topic) {
+
+    override val schema = Event.testStubJsonSerde.asPulsarSchema()
+}
+
+private data class TestEventStream(override val topic: Topic) : EventStream {
+
+    override fun messageKeyForEvent(event: Event) = when (event) {
+        is TestEvent -> event.id.stringValue
+        is TestEntityEvent -> event.entityId.stringValue
+        else -> error("Unsupported event $event")
+    }
+
+    override fun messagePropertiesForEvent(event: Event) = emptyMap<String, String>()
+}
+
+// TODO move
+interface EventStream {
+
+    val topic: Topic
+
+    fun messageKeyForEvent(event: Event): String
+
+    fun messagePropertiesForEvent(event: Event): Map<String, String>
+}
+
+data class InstanceInfo(val id: Name, val groupName: Name)
