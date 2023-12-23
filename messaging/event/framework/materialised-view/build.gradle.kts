2a433a9081b5bd5cdb2d9c499b9afb093469eb09
Michele Sollecito <michele.sollecito@gmail.com>
2023-12-23 17:54:18 +0100

    WIP

--- messaging/event/framework/materialised-view/build.gradle.kts
+++ messaging/event/framework/materialised-view/build.gradle.kts
@@ -2,8 +2,6 @@ dependencies {
     api(projects.chassisDddDomain)
     api(projects.chassisDddLoggingUtils)
     api(projects.chassisLoggerCore)
-    api(projects.chassisJsonUtils) // TODO remove
-    api(projects.chassisPulsarUtils)
     api(projects.chassisMessagingDomain)
 
     testImplementation(projects.chassisDddEventStoreMemory)
