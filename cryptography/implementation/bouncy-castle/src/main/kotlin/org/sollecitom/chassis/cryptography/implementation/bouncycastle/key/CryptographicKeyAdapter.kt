2f90ec96e720f73b61fb8627100b570d7e6a7c27
Michele Sollecito <michele.sollecito@gmail.com>
2024-01-11 10:50:00 +0000

    WIP

--- cryptography/implementation/bouncy-castle/src/main/kotlin/org/sollecitom/chassis/cryptography/implementation/bouncycastle/key/CryptographicKeyAdapter.kt
+++ cryptography/implementation/bouncy-castle/src/main/kotlin/org/sollecitom/chassis/cryptography/implementation/bouncycastle/key/CryptographicKeyAdapter.kt
@@ -1,7 +1,7 @@
 package org.sollecitom.chassis.cryptography.implementation.bouncycastle.key
 
 import org.sollecitom.chassis.cryptography.domain.key.CryptographicKey
-import org.sollecitom.chassis.hashing.utils.murmur3.Murmur3
+import org.sollecitom.chassis.hashing.utils.xxh.Xxh3
 import java.security.Key
 import java.util.*
 
@@ -12,5 +12,5 @@ value class CryptographicKeyAdapter(val key: Key) : CryptographicKey {
     override val encodedAsHexString: String get() = HexFormat.of().formatHex(encoded)
     override val algorithm: String get() = key.algorithm
     override val format: String get() = key.format
-    override val hash: Long get() = Murmur3.hash64(encoded)
+    override val hash: Long get() = Xxh3.hash64(encoded)
 }
