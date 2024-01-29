6c6aedd013e20586df3e81d3bfda3d228f3e9a3b
Michele Sollecito <michele.sollecito@gmail.com>
2024-01-29 10:57:23 +0000

    WIP

--- pulsar/test/utils/src/main/kotlin/org/sollecitom/chassis/pulsar/test/utils/CoreDataGeneratorExtensions.kt
+++ pulsar/test/utils/src/main/kotlin/org/sollecitom/chassis/pulsar/test/utils/CoreDataGeneratorExtensions.kt
@@ -5,9 +5,13 @@ import org.sollecitom.chassis.core.utils.RandomGenerator
 import org.sollecitom.chassis.kotlin.extensions.text.CharacterGroups.lowercaseCaseLetters
 import org.sollecitom.chassis.kotlin.extensions.text.strings
 import org.sollecitom.chassis.pulsar.utils.PulsarTopic
+import org.sollecitom.chassis.pulsar.utils.TenantAgnosticPulsarTopic
 
 context(RandomGenerator)
-fun PulsarTopic.Companion.create(persistent: Boolean = true, namespace: PulsarTopic.Namespace? = PulsarTopic.Namespace(tenant = randomName(), name = randomName()), name: Name = randomName()): PulsarTopic = of(persistent, namespace, name)
+fun PulsarTopic.Companion.create(persistent: Boolean = true, tenant: Name = randomName(), namespaceName: Name = randomName(), name: Name = randomName(), namespace: PulsarTopic.Namespace? = PulsarTopic.Namespace(tenant = tenant, name = namespaceName)): PulsarTopic = of(persistent, namespace, name)
+
+context(RandomGenerator)
+fun TenantAgnosticPulsarTopic.Companion.create(persistent: Boolean = true, name: Name = randomName()) = TenantAgnosticPulsarTopic(name, persistent)
 
 context(RandomGenerator)
 private fun randomName(): Name = random.strings(5..10, lowercaseCaseLetters).iterator().next().let(::Name)
--- pulsar/utils/src/main/kotlin/org/sollecitom/chassis/pulsar/utils/TenantAgnosticPulsarTopic.kt
+++ pulsar/utils/src/main/kotlin/org/sollecitom/chassis/pulsar/utils/TenantAgnosticPulsarTopic.kt
@@ -2,12 +2,14 @@ package org.sollecitom.chassis.pulsar.utils
 
 import org.sollecitom.chassis.core.domain.naming.Name
 
-data class TenantAgnosticPulsarTopic(val name: Name) {
+data class TenantAgnosticPulsarTopic(val name: Name, val persistent: Boolean) {
 
-    fun withTenantAndNamespace(tenantAndNamespace: PulsarTopic.Namespace, persistent: Boolean = true): PulsarTopic = when (persistent) {
+    fun withTenantAndNamespace(tenantAndNamespace: PulsarTopic.Namespace): PulsarTopic = when (persistent) {
         true -> PulsarTopic.Persistent(tenantAndNamespace, name)
         false -> PulsarTopic.NonPersistent(tenantAndNamespace, name)
     }
 
-    fun withoutTenant(persistent: Boolean = true) = PulsarTopic.of(persistent = persistent, namespace = null, name = name)
+    fun withoutTenant() = PulsarTopic.of(persistent = persistent, namespace = null, name = name)
+
+    companion object
 }
