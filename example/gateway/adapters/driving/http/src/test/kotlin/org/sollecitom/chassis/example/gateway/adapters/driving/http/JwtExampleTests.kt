dbf28b8d2ad2de82e43be73f436dbd5b659ecd41
Michele Sollecito <michele.sollecito@gmail.com>
2024-04-22 14:47:16 +0100

    WIP

--- example/gateway/adapters/driving/http/src/test/kotlin/org/sollecitom/chassis/example/gateway/adapters/driving/http/JwtExampleTests.kt
+++ example/gateway/adapters/driving/http/src/test/kotlin/org/sollecitom/chassis/example/gateway/adapters/driving/http/JwtExampleTests.kt
@@ -22,14 +22,14 @@ import org.sollecitom.chassis.logging.standard.configuration.configureLogging
 
 
 @TestInstance(PER_CLASS)
-private class JwtExampleTests {
+private class JwtExampleTests { // CFR https://bitbucket.org/b_c/jose4j/wiki/JWT%20Examples#markdown-header-producing-and-consuming-signed-and-encrypted-jwt-using-rfc8037s-ed25519-eddsa-and-x25519-ecdh
 
     init {
         configureLogging(defaultMinimumLoggingLevel = LoggingLevel.INFO)
     }
 
     @Test
-    fun `encrypted JWT issuance and verification using RFC8037 Ed25519 EdDSA and X25519 ECDH`() { // TODO continue with https://bitbucket.org/b_c/jose4j/wiki/JWT%20Examples#markdown-header-producing-and-consuming-signed-and-encrypted-jwt-using-rfc8037s-ed25519-eddsa-and-x25519-ecdh
+    fun `encrypted JWT issuance and verification using RFC8037 Ed25519 EdDSA and X25519 ECDH`() {
 
         val issuerJwk = OkpJwkGenerator.generateJwk(OctetKeyPairJsonWebKey.SUBTYPE_ED25519).also { it.keyId = "issuer key" }
         val receiverJwk = OkpJwkGenerator.generateJwk(OctetKeyPairJsonWebKey.SUBTYPE_X25519).also { it.keyId = "receiver key" }
